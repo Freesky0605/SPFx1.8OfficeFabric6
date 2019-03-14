e87e259cc972dde0c28a64247c4d9a60684cf2f2
Corey Roth <coreyroth@coreyrosworkmbp.attlocal.net>
2019-03-14 15:49:36 -0500

    Added command bar for an example.

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -2,12 +2,21 @@ import * as React from 'react';
 import styles from './HelloWorld.module.scss';
 import { IHelloWorldProps } from './IHelloWorldProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar';
+
 import { DefaultButton, PrimaryButton, IButtonProps } from 'office-ui-fabric-react/lib/Button';
 
 export default class HelloWorld extends React.Component<IHelloWorldProps, {}> {
   public render(): React.ReactElement<IHelloWorldProps> {
     return (
       <div className={ styles.helloWorld }>
+      <CommandBar
+          items={this.getItems()}
+          overflowItems={this.getOverlflowItems()}
+          overflowButtonProps={{ ariaLabel: 'More commands' }}
+          farItems={this.getFarItems()}
+          ariaLabel={'Use left and right arrow keys to navigate between commands'}
+        />
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
@@ -34,4 +43,125 @@ export default class HelloWorld extends React.Component<IHelloWorldProps, {}> {
   private _alertClicked(): void {
     alert('Clicked');
   }
+
+  private getItems = () => {
+    return [
+      {
+        key: 'newItem',
+        name: 'New',
+        cacheKey: 'myCacheKey', // changing this key will invalidate this items cache
+        iconProps: {
+          iconName: 'Add'
+        },
+        ariaLabel: 'New',
+        subMenuProps: {
+          items: [
+            {
+              key: 'emailMessage',
+              name: 'Email message',
+              iconProps: {
+                iconName: 'Mail'
+              },
+              ['data-automation-id']: 'newEmailButton'
+            },
+            {
+              key: 'calendarEvent',
+              name: 'Calendar event',
+              iconProps: {
+                iconName: 'Calendar'
+              }
+            }
+          ]
+        }
+      },
+      {
+        key: 'upload',
+        name: 'Upload',
+        iconProps: {
+          iconName: 'Upload'
+        },
+        href: 'https://dev.office.com/fabric',
+        ['data-automation-id']: 'uploadButton'
+      },
+      {
+        key: 'share',
+        name: 'Share',
+        iconProps: {
+          iconName: 'Share'
+        },
+        onClick: () => console.log('Share')
+      },
+      {
+        key: 'download',
+        name: 'Download',
+        iconProps: {
+          iconName: 'Download'
+        },
+        onClick: () => console.log('Download')
+      }
+    ];
+  };
+
+  private getOverlflowItems = () => {
+    return [
+      {
+        key: 'move',
+        name: 'Move to...',
+        onClick: () => console.log('Move to'),
+        iconProps: {
+          iconName: 'MoveToFolder'
+        }
+      },
+      {
+        key: 'copy',
+        name: 'Copy to...',
+        onClick: () => console.log('Copy to'),
+        iconProps: {
+          iconName: 'Copy'
+        }
+      },
+      {
+        key: 'rename',
+        name: 'Rename...',
+        onClick: () => console.log('Rename'),
+        iconProps: {
+          iconName: 'Edit'
+        }
+      }
+    ];
+  };
+
+  private getFarItems = () => {
+    return [
+      {
+        key: 'sort',
+        name: 'Sort',
+        ariaLabel: 'Sort',
+        iconProps: {
+          iconName: 'SortLines'
+        },
+        onClick: () => console.log('Sort')
+      },
+      {
+        key: 'tile',
+        name: 'Grid view',
+        ariaLabel: 'Grid view',
+        iconProps: {
+          iconName: 'Tiles'
+        },
+        iconOnly: true,
+        onClick: () => console.log('Tiles')
+      },
+      {
+        key: 'info',
+        name: 'Info',
+        ariaLabel: 'Info',
+        iconProps: {
+          iconName: 'Info'
+        },
+        iconOnly: true,
+        onClick: () => console.log('Info')
+      }
+    ];
+  };
 }
